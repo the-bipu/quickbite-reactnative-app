58b2c084df9f133ec019365828be9ee494ceeb16
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-08 11:39:59 +0530

    Update HomeScreen.js
    Update the previous Header and added Items in the Body Section.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, TextInput } from 'react-native';
+import { View, Text, Image, TextInput, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import React, { useLayoutEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
@@ -8,6 +8,8 @@ import {
     MagnifyingGlassIcon,
     CogIcon,
  } from 'react-native-heroicons/outline';
+import Categories from '../Categories/Categories';
+import FeaturedRow from '../Categories/FeaturedRow';
 
 export default function HomeScreen() {
   const navigation = useNavigation();
@@ -23,7 +25,7 @@ export default function HomeScreen() {
     <SafeAreaView className="bg-white pt-1">
 
         {/* Header */}
-        <View className="flex-row pb-3 items-center mx-4 space-x-2 px-3">
+        <View className="flex-row pb-3 mx-1 items-center space-x-2 px-3">
           <Image
               source={{
                 uri: 'https://cdn-icons-png.flaticon.com/512/6833/6833605.png'
@@ -46,7 +48,7 @@ export default function HomeScreen() {
           <UserIcon size={35} color="#00CCBB" />
         </View>
 
-        <View  className="flex-row items-center space-x-2 pb-2 mx-4 px-3">
+        <View  className="flex-row items-center space-x-2 pb-2 mx-1 px-3">
           <View className="flex-row space-x-2 p-3 flex-1 bg-gray-300">
             <MagnifyingGlassIcon color="gray" size={20} />
             <TextInput
@@ -59,6 +61,39 @@ export default function HomeScreen() {
           
           <CogIcon color="#00CCBB" />
         </View>
+
+        {/* Body */}
+        <ScrollView 
+          className="bg-gray-100"
+          contentContainerStyle={{
+            paddingBottom: 100,
+          }}
+        >
+          {/* Categories */}
+          <Categories />
+
+          {/* Featured */}
+          <FeaturedRow
+          id="123"
+            title="Featured"
+            description="Paid Placements for our partners"
+          />
+
+          {/* Tasty Discounts */}
+          <FeaturedRow
+          id="124"
+            title="Tasty Discounts"
+            description="Paid Placements for our partners"
+          />
+
+          {/* Offers near you */}
+          <FeaturedRow
+          id="125"
+            title="Offers near you"
+            description="Paid Placements for our partners"
+          />
+
+        </ScrollView>
 
     </SafeAreaView>
   )
