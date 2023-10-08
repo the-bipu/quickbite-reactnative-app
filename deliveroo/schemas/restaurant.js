54b830e76712a5dfac4299d856ebae94d1bb2f03
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-08 22:58:56 +0530

    Create Schemas
    Created Schemas for every items we need on homepage. Then exports it using index.js file so that we can use it directly from the frontend part.

---
+++ deliveroo/schemas/restaurant.js
@@ -0,0 +1,65 @@
+import { defineType } from 'sanity'
+
+export default defineType({
+  name: 'restaurant',
+  title: 'Restaurant',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title : 'Restaurant Name',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'short_description',
+      type: "string",
+      title: 'short_description',
+      validation: (Rule) => Rule.max(200),
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Image of the Restaurant',
+    },
+    {
+      name: 'lat',
+      type: 'number',
+      title: 'Latitute of the Restaurant',
+    },
+    {
+      name: 'long',
+      type: 'number',
+      title: 'Longitude of the Restaurant',
+    },
+    {
+      name: 'address',
+      type: 'string',
+      title: 'Restaurant address',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'rating',
+      type: 'number',
+      title: 'Enter a Rating from (1-5 Stars)',
+      validation: (Rule) =>
+        Rule.required()
+         .min(1)
+         .max(5)
+         .error("Please enter a value between 1 to 5"),
+    },
+    {
+      name: 'type',
+      title: 'category',
+      validation: (Rule) => Rule.required(),
+      type: 'reference',
+      to: [{ type: 'category' }],
+    },
+    {
+      name: 'dishes',
+      type: 'array',
+      title: 'Dishes',
+      of: [{type: 'reference', to: [{ type: 'dish'}] }],
+    },
+  ],
+})
