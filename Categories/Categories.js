117722c184fb49ba9d97d85b9c35bd2d1eb54994
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-08 11:40:42 +0530

    Create Categories.js
    First Section of the Homepage, here all the categories of food items are defined.

---
+++ Categories/Categories.js
@@ -0,0 +1,48 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+import CategoryCard from './CategoryCard';
+
+export default function Categories() {
+  return (
+    <ScrollView
+        contentContainerStyle={{
+            paddingHorizontal: 15,
+            paddingTop: 10,
+        }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+    >
+
+      <CategoryCard 
+        imgUrl="https://images.unsplash.com/photo-1598214886806-c87b84b7078b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1325&q=80" 
+        title="Trial 1" 
+      />
+
+      <CategoryCard 
+        imgUrl="https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1380&q=80" 
+        title="Trial 1" 
+      />
+
+      <CategoryCard 
+        imgUrl="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1381&q=80" 
+        title="Trial 1" 
+      />
+
+      <CategoryCard 
+        imgUrl="https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1365&q=80" 
+        title="Trial 1" 
+      />
+
+      <CategoryCard 
+        imgUrl="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" 
+        title="Trial 1" 
+      />
+
+      <CategoryCard 
+        imgUrl="https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1310&q=80" 
+        title="Trial 1" 
+      />
+
+    </ScrollView>
+  )
+}
