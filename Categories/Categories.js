64e25fe27d05ccbbe9a9dce4de11f1a1340f5a42
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-10 22:00:17 +0530

    Update Categories.js
    Added dynamic link for fetching data from sanity in the categories.js file.

--- Categories/Categories.js
+++ Categories/Categories.js
@@ -1,8 +1,21 @@
-import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from 'react-native';
+import React, { useState, useEffect } from 'react';
 import CategoryCard from './CategoryCard';
+import client, { urlFor } from '../sanity';
 
 export default function Categories() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    client.fetch(
+      `
+      *[_type == "category"]
+      `
+    ).then((data) => {
+      setCategories(data);
+    });
+  }, []);
+
   return (
     <ScrollView
         contentContainerStyle={{
@@ -13,12 +26,15 @@ export default function Categories() {
         showsHorizontalScrollIndicator={false}
     >
 
-      <CategoryCard 
-        imgUrl="https://images.unsplash.com/photo-1598214886806-c87b84b7078b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1325&q=80" 
-        title="Trial 1" 
-      />
+      {categories?.map((item) => (
+        <CategoryCard 
+          key={item._id}
+          imgUrl={urlFor(item.image).width(200).url()} 
+          title={item.name}
+        />
+      ))}
 
-      <CategoryCard 
+      {/* <CategoryCard 
         imgUrl="https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1380&q=80" 
         title="Trial 1" 
       />
@@ -41,7 +57,7 @@ export default function Categories() {
       <CategoryCard 
         imgUrl="https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1310&q=80" 
         title="Trial 1" 
-      />
+      /> */}
 
     </ScrollView>
   )
