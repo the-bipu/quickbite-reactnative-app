552c8cfd913a3526b23b8ecc635630872c552f55
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-06 02:08:32 +0530

    Update App.js

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import RestaurantScreen from './screens/RestaurantScreen';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import BasketScreen from './screens/BasketScreen';
+import PreparingOrderScreen from './screens/PreparingOrderScreen';
+import DeliveryScreen from './screens/DeliveryScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -21,6 +23,16 @@ export default function App() {
               name="Basket" 
               component={BasketScreen} 
               options={{ presentation: "modal", headerShown: false }}
+            />
+            <Stack.Screen 
+              name="PreparingOrderScreen" 
+              component={PreparingOrderScreen}
+              options={{ presentation: "fullScreenModal", headerShown: false }}
+            />
+            <Stack.Screen 
+              name="Delivery" 
+              component={DeliveryScreen}
+              options={{ presentation: "fullScreenModal", headerShown: false }}
             />
           </Stack.Navigator>
         </Provider>
